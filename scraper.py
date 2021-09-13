f0e0e4cac4cc7af7eea5c8faa038db23a73535f7
griffincorriher <57311125+griffincorriher@users.noreply.github.com>
2021-09-13 01:16:43 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 #import libraries
-import datetime as dt
+from datetime import date, datetime, time
+import time
 import mysql.connector as mysql
 from bs4 import BeautifulSoup
 import requests
@@ -19,12 +20,14 @@ def scrape():
     global info8
     global info9
     global info10    
+    max = 100
     
     session = HTMLSession()
     resp = session.get("https://parkingavailability.uncc.edu/")
     resp.html.render()
     soup = BeautifulSoup(resp.html.html, "lxml")
-
+    resp.html.render(timeout=20)
+    
     #Get required info
     allInfo = soup.find_all("mat-list-item")
     allInfoList = [tag.text for tag in allInfo]
@@ -48,60 +51,51 @@ def scrape():
 
         
 def database():
-    now = dt.datetime.now()
-    now = now.strftime('%Y-%m-%d %H:%M:%S')
+    now = datetime.now()
+    time = now.strftime('%H:%M:%S')
+    date = now.strftime('%Y-%m-%d')
+    day = now.strftime('%A')
     db = mysql.connect(
         host = "localhost",
         user = "root",
         passwd = "Letter69!",
-        database = "parkinginformation2"
+        database = "parkinginformation"
     )
     #print(db)
     cursor = db.cursor()
     
-    #cursor.execute("CREATE DATABASE parkinginformation2")
-    
-    #cursor.execute("DROP TABLE parking")
-    
-    #cursor.execute("CREATE TABLE Parking (nameID int PRIMARY KEY AUTO_INCREMENT, date VARCHAR(100), name VARCHAR(100), cap smallint UNSIGNED)")
-    
-    #cursor.execute("DESCRIBE Parking")
-    #for i in cursor:
-    #    print(i)
-
-    sql = "INSERT INTO Parking(date, name, cap) VALUES(%s, %s, %s)"
+    sql = "INSERT INTO parking (date, day, time, name, cap) VALUES (%s, %s, %s, %s, %s)"
     query = "SELECT * FROM parking"
+    dropDB = "DROP DATABASE parkinginformation2"
+    createDB = "CREATE DATABASE parkinginformation"
+    dropTable = "DROP TABLE parking"
+    createTable = "CREATE TABLE parking (ID int PRIMARY KEY AUTO_INCREMENT, date VARCHAR(100), day VARCHAR(10), time VARCHAR(50), name VARCHAR(100), cap smallint UNSIGNED)"
+    showTable = "DESCRIBE parking"
+    deleteQuery = "DELETE FROM parking"
     val = [
-        (now, info1[2].replace('star_border','').replace(' ',''), int(info1[1].replace('%',''))),
-        (now, info2[2].replace('star_border','').replace(' ',''), int(info2[1].replace('%',''))),
-        (now, info3[2].replace('star_border','').replace(' ',''), int(info3[1].replace('%',''))),
-        (now, info4[2].replace('star_border','').replace(' ',''), int(info4[1].replace('%',''))),
-        (now, info5[2].replace('star_border','').replace(' ',''), int(info5[1].replace('%',''))),
-        (now, info6[2].replace('star_border','').replace(' ',''), int(info6[1].replace('%',''))),
-        (now, info7[2].replace('star_border','').replace(' ',''), int(info7[1].replace('%',''))),
-        (now, info8[2].replace('star_border','').replace(' ',''), int(info8[1].replace('%',''))),
-        (now, info9[2].replace('star_border','').replace(' ',''), int(info9[1].replace('%',''))),
-        (now, info10[2].replace('star_border','').replace(' ',''), int(info10[1].replace('%','')))
+        (date, day, time, info1[2].replace('star_border','').replace(' ',''), 100-int(info1[1].replace('%',''))),
+        (date, day, time, info2[2].replace('star_border','').replace(' ',''), 100-int(info2[1].replace('%',''))),
+        (date, day, time, info3[2].replace('star_border','').replace(' ',''), 100-int(info3[1].replace('%',''))),
+        (date, day, time, info4[2].replace('star_border','').replace(' ',''), 100-int(info4[1].replace('%',''))),
+        (date, day, time, info5[2].replace('star_border','').replace(' ',''), 100-int(info5[1].replace('%',''))),
+        (date, day, time, info6[2].replace('star_border','').replace(' ',''), 100-int(info6[1].replace('%',''))),
+        (date, day, time, info7[2].replace('star_border','').replace(' ',''), 100-int(info7[1].replace('%',''))),
+        (date, day, time, info8[2].replace('star_border','').replace(' ',''), 100-int(info8[1].replace('%',''))),
+        (date, day, time, info9[2].replace('star_border','').replace(' ',''), 100-int(info9[1].replace('%',''))),
+        (date, day, time, info10[2].replace('star_border','').replace(' ',''), 100-int(info10[1].replace('%','')))
     ]
+
+    #cursor.execute(createTable)
     
     cursor.executemany(sql,val)
-    db.commit
-    print(cursor.rowcount, "record(s) inserted.")
+    db.commit()
+    #print(cursor.rowcount, "record(s) inserted.")
+    #db.close()
 
-    
-    cursor.execute("SELECT * FROM Parking")
-
-    result = cursor.fetchall()
-    for i in result:
-        print(i)
-    
     #cursor.execute(query)
-    #results = cursor.fetchall()
-    #for i in results:
-    #   print(i)
-
-
-    
+    #result = cursor.fetchall()
+    #for i in result:
+    #    print(i)
 
 def parkingdeck(allInfoList):
     print("Parking deck options")
@@ -144,6 +138,7 @@ def parkingdeck(allInfoList):
     else:
         info11 = allInfoList[10].split("\n")
         print(info11[2].replace('star_border',''),":",info[1])
+
 
 
 
